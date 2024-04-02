ed77f78692698acf895e259a6fee864363fa6f19
prateek041 <prateeksingh9741@gmail.com>
2024-04-02 16:35:51 +0530

    Add testCoreToolArguments function to core assistant

--- node/src/openai/assistants/core/functions.ts
+++ node/src/openai/assistants/core/functions.ts
@@ -1,1 +1,28 @@
 // list of all the functions supported by the core assistant.
+export interface ITestCoreToolArguments {
+    type: "testCoreToolArguments";
+    // the type of the core tool arguments
+    coreToolArguments: any;
+    // the core tool arguments
+
+}
+
+export const testCoreToolFunction = async ()=>{
+    console.log("testing core tool function")
+}
+export type CoreToolArguments = ITestCoreToolArguments
+
+const testFunction = {
+    name: "testCoreToolArguments",
+    function: testCoreToolFunction
+}
+
+export const coreAssistantFunctions = {
+    testFunction
+}
+
+
+// TODO: improve the naming convention of the functions.
+export const getCoreAssistantFunctions = () => {
+    return coreAssistantFunctions
+}

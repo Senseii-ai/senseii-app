f418a5cbb0cd9e9917c3547efe284750f9fc94e2
prateek041 <prateeksingh9741@gmail.com>
2024-04-14 13:21:56 +0530

    Adding demo openAPI spec

--- node/src/routes/chat.ts
+++ node/src/routes/chat.ts
@@ -9,9 +9,38 @@ const router: Router = express.Router()
 // /continue: continues the chat if id is provided
 // /testChat: is deprecated
 
-//router.route("/").get(chat)
-router.route("/start").post(startChat)
 router.route("/continue").post(continueChat)
 router.route("/testChat").post(chatCore)
+
+/**
+  * @openapi
+  * /startChat
+  * post:
+  *   summary: Starts a new chat using the input message passed by the user.
+  *   requestBody:
+  *     required: true
+  *     content:
+  *       application/json:
+  *         schema:
+  *           type: object
+  *           properties:
+  *             message:
+  *               type: object
+  *               description: The message to start the chat.
+  *               properties:
+  *               role:
+  *                 type: string
+  *                 enum: "user"
+  *                 description: The role of the user.
+  *               content:
+  *                 type: string
+  *                 description: the content of the message
+  *   response:
+  *     200:
+  *       description: Chat created successfully
+  *       content: application/json
+  *       # remaining here
+  */
+router.route("/startChat").post(startChat)
 
 export default router

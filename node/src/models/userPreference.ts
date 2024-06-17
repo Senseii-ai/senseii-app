f0d60a01268a3342e6cd2ef804cdf032c6941b32
prateek041 <prateeksingh9741@gmail.com>
2024-06-17 23:48:47 +0530

    adding types and models for user preferences

---
+++ node/src/models/userPreference.ts
@@ -0,0 +1,132 @@
+import { Schema, model } from "mongoose";
+import {
+  IBasicInformation,
+  ILifeStyle,
+  IConstraints,
+  IHealthGoals,
+  IEatingHabits,
+  IDietPreferences,
+  IUserPreferences,
+} from "../types/userPreference";
+
+interface IUserPreferencesDocument extends IUserPreferences, Document { }
+interface IBasicInformationDocument extends IBasicInformation, Document { }
+interface ILifeStyleDocument extends ILifeStyle, Document { }
+interface IDietPreferencesDocument extends IDietPreferences, Document { }
+interface IHealthGoalsDocument extends IHealthGoals, Document { }
+interface IEatingHabitsDocument extends IEatingHabits, Document { }
+interface IConstraintsDocument extends IConstraints, Document { }
+
+const IBasicInformationSchema: Schema<IBasicInformationDocument> = new Schema({
+  age: Number,
+  weight: {
+    value: Number,
+    unit: {
+      type: String,
+      enum: ["Kilograms", "Grams", "Pounds"],
+    },
+  },
+  height: {
+    value: Number,
+    unit: {
+      type: String,
+      enum: ["Centimeters"],
+    },
+  },
+  gender: String,
+
+})
+
+const ILifeStyleSchema: Schema<ILifeStyleDocument> = new Schema({
+  dailyRoutine: {
+    type: String,
+    enum: ["sedenatry", "light", "moderate", "heavy", "very heavy"],
+  },
+  exerciseRoutine: [
+    {
+      exerciseType: {
+        type: String,
+        enum: ["cardio", "strength", "flexibility", "balance", "none"],
+      },
+      frequency: {
+        type: String,
+        enum: ["daily", "weekly", "monthly"],
+      },
+    },
+  ],
+
+})
+
+const IDietPreferencesSchema: Schema<IDietPreferencesDocument> = new Schema({
+  preference: {
+    type: String,
+    enum: [
+      "vegetarian",
+      "non-vegetarian",
+      "vegan",
+      "pescatarian",
+      "omnivore",
+      "ketogenic",
+      "paleo",
+    ],
+  },
+  allergies: [String],
+  intolerances: [String],
+  dislikedFood: [String],
+  favouriteFood: [String],
+})
+
+const IHealthGoalsSchema: Schema<IHealthGoalsDocument> = new Schema({
+  weightGoal: {
+    type: String,
+    enum: ["gain", "loss", "maintain"],
+  },
+  specificNutritionGoal: String,
+  medicalConditions: String,
+})
+
+const IEatingHabitsSchema: Schema<IEatingHabitsDocument> = new Schema({
+  mealsPerDay: Number,
+  mealComplexity: {
+    type: String,
+    enum: ["daily", "weekly", "monthly"],
+  },
+  cookingTime: {
+    type: String,
+    enum: ["less than 30 minutes", "30-60 minutes", "more than 60 minutes"],
+  },
+
+})
+
+const IConstraintsSchema: Schema<IConstraintsDocument> = new Schema({
+  financial: {
+    budget: Number,
+    budgetType: {
+      type: String,
+      enum: ["daily", "weekly", "monthly"],
+    },
+  },
+  geographical: {
+    location: String,
+  },
+})
+
+export const UserPreferencesSchema: Schema<IUserPreferencesDocument> = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "Users",
+    required: true,
+  },
+  basicInformation: { type: IBasicInformationSchema, requried: true },
+  lifeStyle: { type: ILifeStyleSchema, requried: true },
+  dietPreferences: { type: IDietPreferencesSchema, requried: true },
+  healthGoals: { type: IHealthGoalsSchema, requried: true },
+  eatingHabits: { type: IEatingHabitsSchema, requried: true },
+  constraints: { type: IConstraintsSchema, requried: true }
+})
+
+export const UserPreferencesModel = model<IUserPreferencesDocument>(
+  "UserPreferences",
+  UserPreferencesSchema
+)
+
---
+++ node/src/types/userPreference.ts
@@ -0,0 +1,88 @@
+import { Schema } from "mongoose";
+
+export interface IHealthPlan {
+  userId: {
+    type: Schema.Types.ObjectId;
+    ref: "Users";
+    required: true;
+  };
+}
+
+// this interface is for saving the user preference in the database
+export interface IUserPreferences {
+  user: Schema.Types.ObjectId;
+  type: "userPreferences";
+  basicInformation: IBasicInformation;
+  lifeStyle: ILifeStyle;
+  dietPreferences: IDietPreferences;
+  healthGoals: IHealthGoals;
+  eatingHabits: IEatingHabits;
+  constraints: IConstraints;
+}
+
+
+
+export interface IBasicInformation {
+  age: number;
+  weight: {
+    value: number;
+    unit: "Kilograms" | "Grams" | "Pounds";
+  };
+  height: {
+    value: number;
+    unit: "Centimeters";
+  };
+  gender: string;
+}
+
+export interface ILifeStyle {
+  dailyRoutine: "sedenatry" | "light" | "moderate" | "heavy" | "very heavy";
+  exerciseRoutine?: [
+    {
+      exerciseType: "cardio" | "strength" | "flexibility" | "balance" | "none";
+      frequency: "daily" | "weekly" | "monthly";
+    },
+  ];
+}
+
+export interface IDietPreferences {
+  preference:
+  | "vegetarian"
+  | "non-vegetarian"
+  | "vegan"
+  | "pescatarian"
+  | "omnivore"
+  | "ketogenic"
+  | "paleo";
+  allergies: string[];
+  intolerances: string[];
+  dislikedFood?: string[];
+  favouriteFood?: string[];
+}
+
+export interface IHealthGoals {
+  weightGoal?: "gain" | "loss" | "maintain";
+  specificNutritionGoal: string;
+  medicalConditions: string[];
+}
+
+export interface IEatingHabits {
+  mealsPerDay: number;
+  mealComplexity: "simple" | "moderate" | "complex";
+  cookingTime:
+  | "less than 30 minutes"
+  | "30-60 minutes"
+  | "more than 60 minutes";
+}
+
+export interface IConstraints {
+  financial: {
+    budget: number;
+    budgetType: "daily" | "weekly" | "monthly";
+  };
+  geographical: {
+    location: string;
+  };
+}
+
+

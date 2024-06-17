57f0d7b975db9cd94a60ccccc874f634f51edaf5
prateek041 <prateeksingh9741@gmail.com>
2024-06-17 23:49:17 +0530

    cleanup: removing type definition from nutrition assistant function

--- node/src/openai/assistants/nutrition/functions.ts
+++ node/src/openai/assistants/nutrition/functions.ts
@@ -18,69 +18,6 @@ const openAIClient = getOpenAIClient();
  * height: IBodyMeasurement
  *
  */
-export interface IBasicInformation {
-  age: number;
-  weight: {
-    value: number;
-    unit: "Kilograms" | "Grams" | "Pounds";
-  };
-  height: {
-    value: number;
-    unit: "Centimeters";
-  };
-  gender: string;
-}
-
-export interface ILifeStyle {
-  dailyRoutine: "sedenatry" | "light" | "moderate" | "heavy" | "very heavy";
-  exerciseRoutine?: [
-    {
-      exerciseType: "cardio" | "strength" | "flexibility" | "balance" | "none";
-      frequency: "daily" | "weekly" | "monthly";
-    },
-  ];
-}
-
-export interface IDietPreferences {
-  preference:
-    | "vegetarian"
-    | "non-vegetarian"
-    | "vegan"
-    | "pescatarian"
-    | "omnivore"
-    | "ketogenic"
-    | "paleo";
-  allergies: string[];
-  intolerances: string[];
-  dislikedFood?: string[];
-  favouriteFood?: string[];
-}
-
-export interface IHealthGoals {
-  weightGoal?: "gain" | "loss" | "maintain";
-  specificNutritionGoal: string;
-  medicalConditions: string[];
-}
-
-export interface IEatingHabits {
-  mealsPerDay: number;
-  mealComplexity: "simple" | "moderate" | "complex";
-  cookingTime:
-    | "less than 30 minutes"
-    | "30-60 minutes"
-    | "more than 60 minutes";
-}
-
-export interface IConstraints {
-  financial: {
-    budget: number;
-    budgetType: "daily" | "weekly" | "monthly";
-  };
-  geographical: {
-    location: string;
-  };
-}
-
 // wrapper function.
 export const CreateNutritionPlan = async (
   functionArguments: ICreateNutritionPlanArguments,

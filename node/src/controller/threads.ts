0727e827ffdf2cf7c9c7972b1d9fb295b0ec7ddd
Prateek Singh <prateeksingh9741@gmail.com>
2024-09-01 13:54:18 +0530

    temporary test route

--- node/src/controller/threads.ts
+++ node/src/controller/threads.ts
@@ -8,14 +8,83 @@ import { Response } from "express";
 
 const client = getOpenAIClient();
 
+const sampleThreadMessages = [
+  {
+    role: "user",
+    message: "Hello I need help",
+  },
+  {
+    role: "assistant",
+    message: "What can I help you with",
+  },
+  {
+    role: "user",
+    message: "I want to loose weight",
+  },
+  {
+    role: "assistant",
+    message: "Please share more informatino about yourself",
+  },
+
+  {
+    role: "user",
+    message: "Hello I need help",
+  },
+  {
+    role: "assistant",
+    message: "What can I help you with",
+  },
+  {
+    role: "user",
+    message: "I want to loose weight",
+  },
+  {
+    role: "assistant",
+    message: "Please share more informatino about yourself",
+  },
+  {
+    role: "user",
+    message: "Hello I need help",
+  },
+  {
+    role: "assistant",
+    message: "What can I help you with",
+  },
+  {
+    role: "user",
+    message: "I want to loose weight",
+  },
+  {
+    role: "assistant",
+    message: "Please share more informatino about yourself",
+  },
+  {
+    role: "user",
+    message: "Hello I need help",
+  },
+  {
+    role: "assistant",
+    message: "What can I help you with",
+  },
+  {
+    role: "user",
+    message: "I want to loose weight",
+  },
+  {
+    role: "assistant",
+    message: "Please share more informatino about yourself",
+  },
+];
+
 // createEmptyThread creates a new thread and returns the thread object.
 export const createEmptyThread = async (req: IAuthRequest, res: Response) => {
   const response = await getNewEmptyThread(client);
   return res.status(200).json(response);
 };
 
+// FIX: Fix this
 export const getThreadMessaegs = async (req: IAuthRequest, res: Response) => {
-  const { threadId } = req.body;
-  const messages = await retrieveMessages(threadId, client);
-  return res.status(200).json(messages);
+  // const { threadId } = req.body;
+  // const messages = await retrieveMessages(threadId, client);
+  return res.status(200).json(sampleThreadMessages);
 };

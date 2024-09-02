375430c6535028c334b4b9f11bda1e2076f6afdf
Prateek Singh <prateeksingh9741@gmail.com>
2024-09-02 22:16:23 +0530

    threads router cleanup and adding methods to get messages

--- node/src/controller/threads.ts
+++ node/src/controller/threads.ts
@@ -5,76 +5,10 @@ import {
 } from "../services/openai/assistants/threads";
 import { IAuthRequest } from "../middlewares/auth";
 import { Response } from "express";
+import UserProfileModel, { getUserThreads } from "../models/userInfo";
+import { MessagesPage } from "openai/resources/beta/threads/messages";
 
 const client = getOpenAIClient();
-
-const sampleThreadMessages = [
-  {
-    role: "user",
-    message: "Hello I need help",
-  },
-  {
-    role: "assistant",
-    message: "What can I help you with",
-  },
-  {
-    role: "user",
-    message: "I want to loose weight",
-  },
-  {
-    role: "assistant",
-    message: "Please share more informatino about yourself",
-  },
-
-  {
-    role: "user",
-    message: "Hello I need help",
-  },
-  {
-    role: "assistant",
-    message: "What can I help you with",
-  },
-  {
-    role: "user",
-    message: "I want to loose weight",
-  },
-  {
-    role: "assistant",
-    message: "Please share more informatino about yourself",
-  },
-  {
-    role: "user",
-    message: "Hello I need help",
-  },
-  {
-    role: "assistant",
-    message: "What can I help you with",
-  },
-  {
-    role: "user",
-    message: "I want to loose weight",
-  },
-  {
-    role: "assistant",
-    message: "Please share more informatino about yourself",
-  },
-  {
-    role: "user",
-    message: "Hello I need help",
-  },
-  {
-    role: "assistant",
-    message: "What can I help you with",
-  },
-  {
-    role: "user",
-    message: "I want to loose weight",
-  },
-  {
-    role: "assistant",
-    message: "Please share more informatino about yourself",
-  },
-];
 
 // createEmptyThread creates a new thread and returns the thread object.
 export const createEmptyThread = async (req: IAuthRequest, res: Response) => {
@@ -82,9 +16,25 @@ export const createEmptyThread = async (req: IAuthRequest, res: Response) => {
   return res.status(200).json(response);
 };
 
-// FIX: Fix this
+// getThreadMessages retruns a list of all the messages
 export const getThreadMessaegs = async (req: IAuthRequest, res: Response) => {
-  // const { threadId } = req.body;
-  // const messages = await retrieveMessages(threadId, client);
-  return res.status(200).json(sampleThreadMessages);
+  try {
+    const threadId = req.params.id;
+    const response: MessagesPage = await retrieveMessages(threadId, client);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    console.error("Error getting thread messages", error);
+    res.status(501).json({ message: "Internal server error" });
+  }
+};
+
+export const getThreads = async (req: IAuthRequest, res: Response) => {
+  try {
+    const user = req.userId as string;
+    const threads = await getUserThreads(user);
+    return res.status(200).json(threads);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: "Error getting user threads" });
+  }
 };

ddf06373519b86cc01d730cd7c2dcdc2472bd0b4
Prateek Singh <prateeksingh9741@gmail.com>
2024-11-16 19:49:25 +0530

    fix: minor cleanup

--- node/src/services/openai/openai.client.ts
+++ node/src/services/openai/openai.client.ts
@@ -3,10 +3,6 @@ const ApiKey = process.env.AZURE_OPENAI_API_KEY;
 const Endpoint = process.env.AZURE_OPENAI_ENDPOINT;
 const apiVersion = process.env.API_VERSION;
 
-console.log('APIKEY', ApiKey)
-console.log('Endpoint', Endpoint)
-console.log('apiVersion', apiVersion)
-
 const openAIClient = new AzureOpenAI({
   endpoint: Endpoint,
   apiKey: ApiKey,

70f871f0fedeb9c1f9ea338e88a6a6925c471957
Prateek Singh <prateeksingh9741@gmail.com>
2024-11-23 16:53:50 +0530

    modify types to split them into smaller chunks

--- node/src/types/user/goals.ts
+++ node/src/types/user/goals.ts
@@ -12,14 +12,22 @@ export const UserGoalDTO = z.object({
   user: z.string().optional(),
   goalName: z.string(),
   description: z.string(),
-  preferences: userPreferencesValidatorObject,
-  nutritionPlan: nutritionPlanObject,
-  workoutPlan: z.string(), // work in progress.
-  startDate: z.date(),
-  endDate: z.date().optional(),
-  threads: threadsObject
+  preferences: userPreferencesValidatorObject.optional(),
+  nutritionPlan: nutritionPlanObject.optional(),
+  workoutPlan: z.string().optional(), // work in progress.
+  startDate: z.string().optional(),
+  endDate: z.string().optional(),
+  threads: threadsObject.optional()
 })
 
+export const InitialGoal = z.object({
+  user: z.string().optional(),
+  goalName: z.string(),
+  description: z.string(),
+  endDate: z.string().optional()
+})
+
+export type IInitialGoal = z.infer<typeof InitialGoal>
 
 export type IUserGoal = z.infer<typeof UserGoalDTO>
 export type IThreads = z.infer<typeof threadsObject>
--- node/src/types/user/userPreferences.ts
+++ node/src/types/user/userPreferences.ts
@@ -1,7 +1,6 @@
-import { Schema } from "mongoose";
 import { z } from "zod"
 
-const basicInformation = z.object({
+export const basicInformation = z.object({
   age: z.number(),
   weight: z.object({
     value: z.number(),
@@ -22,7 +21,7 @@ const lifeStyle = z.object({
   }))
 })
 
-const dietPreferences = z.object({
+export const dietPreferences = z.object({
   preference: z.enum(["vegetarian", "non-vegetarian", "vegan", "pescatarian", "omnivore", "ketogenic", "paleo"]),
   allergies: z.array(z.string()),
   intolerances: z.array(z.string()),
@@ -31,18 +30,19 @@ const dietPreferences = z.object({
 })
 
 const healthGoals = z.object({
-  weightGoal: z.enum(["gain", "loss", "maintain"]).optional(),
+  weightGoal: z.enum(["gain", "loss", "maintain"]),
   specificNutritionGoal: z.string(),
-  medicalConditions: z.array(z.string())
+  // TODO: medical conditions need better handling.
+  medicalConditions: z.string()
 })
 
-const eatingHabits = z.object({
+export const eatingHabits = z.object({
   mealsPerDay: z.number(),
   mealComplexity: z.enum(["simple", "moderate", "complex"]),
   cookingTime: z.enum(["less than 30 minutes", "30-60 minutes", "more than 60 minutes"])
 })
 
-const constraints = z.object({
+export const constraints = z.object({
   financial: z.object({
     budget: z.number(),
     budgetType: z.enum(["daily", "weekly", "monthly"])
@@ -62,89 +62,10 @@ export const userPreferencesValidatorObject = z.object({
   constraints
 })
 
-// TODO: Migrate away from Interfaces to Types slowly.
-export interface IUserPreferences {
-  user: Schema.Types.ObjectId;
-  type: "userPreferences";
-  basicInformation: IBasicInformation;
-  lifeStyle: ILifeStyle;
-  dietPreferences: IDietPreferences;
-  healthGoals: IHealthGoals;
-  eatingHabits: IEatingHabits;
-  constraints: IConstraints;
-}
-
-
-export interface IHealthPlan {
-  userId: {
-    type: Schema.Types.ObjectId;
-    ref: "Users";
-    required: true;
-  };
-}
-
-
-export interface IBasicInformation {
-  age: number;
-  weight: {
-    value: number;
-    unit: "Kilograms" | "Grams" | "Pounds";
-  };
-  height: {
-    value: number;
-    unit: "Centimeters";
-  };
-  gender: string;
-}
-
-export interface ILifeStyle {
-  dailyRoutine: "sedenatry" | "light" | "moderate" | "heavy" | "very heavy";
-  exerciseRoutine?: [
-    {
-      exerciseType: "cardio" | "strength" | "flexibility" | "balance" | "none";
-      frequency: "daily" | "weekly" | "monthly";
-    },
-  ];
-}
-
-export interface IDietPreferences {
-  preference:
-  | "vegetarian"
-  | "non-vegetarian"
-  | "vegan"
-  | "pescatarian"
-  | "omnivore"
-  | "ketogenic"
-  | "paleo";
-  allergies: string[];
-  intolerances: string[];
-  dislikedFood?: string[];
-  favouriteFood?: string[];
-}
-
-export interface IHealthGoals {
-  weightGoal?: "gain" | "loss" | "maintain";
-  specificNutritionGoal: string;
-  medicalConditions: string[];
-}
-
-export interface IEatingHabits {
-  mealsPerDay: number;
-  mealComplexity: "simple" | "moderate" | "complex";
-  cookingTime:
-  | "less than 30 minutes"
-  | "30-60 minutes"
-  | "more than 60 minutes";
-}
-
-export interface IConstraints {
-  financial: {
-    budget: number;
-    budgetType: "daily" | "weekly" | "monthly";
-  };
-  geographical: {
-    location: string;
-  };
-}
-
-
+export type IUserPreferences = z.infer<typeof userPreferencesValidatorObject>
+export type IBasicInformation = z.infer<typeof basicInformation>
+export type ILifeStyle = z.infer<typeof lifeStyle>
+export type IDietPreferences = z.infer<typeof dietPreferences>
+export type IHealthGoals = z.infer<typeof healthGoals>
+export type IEatingHabits = z.infer<typeof eatingHabits>
+export type IConstraints = z.infer<typeof constraints>

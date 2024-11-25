8af379745f8cc0136ba516533f39efea11f4b014
Prateek Singh <prateeksingh9741@gmail.com>
2024-11-25 18:55:00 +0530

    fix: refactor Auth routes and structure api responses

--- node/src/models/users.ts
+++ node/src/models/users.ts
@@ -1,42 +1,72 @@
 import mongoose, { Schema } from "mongoose";
-import { User } from "@senseii/types"
 import { infoLogger } from "@utils/logger";
+import { CreateUserRequest, UserModelSchema } from "@senseii/types";
+import { getSalt, hashPassword } from "@utils/crypt";
 
-const UserSchema: Schema = new Schema<User>({
+const UserSchema: Schema = new Schema<UserModelSchema>({
   email: {
     type: String,
     unique: true,
     required: [true, "Email must be provided"],
   },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String
+  },
   password: {
     type: String,
     required: true,
   },
-  salt: {
+  passwordSalt: {
     type: String,
     required: true,
   },
-  accessToken: {
-    type: String,
-    unique: true,
+  lastLoginAt: {
+    type: Date,
+    required: true
   },
+  createdAt: {
+    type: Date,
+    required: true
+  }
 });
 
 export const getUserByEmail = async (email: string) => {
-  const user = await UserModel.findOne({ email: email });
-  if (!user) {
-    infoLogger({ status: "failed", message: "Error finding user" })
-    throw new Error("Error finding user");
+  return await UserModel.findOne({ email: email })
+}
+
+export const saveNewUser = async (user: CreateUserRequest) => {
+  try {
+    const salt = getSalt()
+    const hashedPassword: string = await hashPassword(user.password, salt);
+    const newUser = await (new UserModel({ ...user, lastLoginAt: new Date, passwordSalt: salt, password: hashedPassword }).save())
+    infoLogger({ status: "INFO", message: `saved ${user} in db` })
+    return newUser
+  } catch (error) {
+    infoLogger({ status: "failed", message: "unable to save ${user} in db" })
+    throw error
   }
-  const userDTO: User = {
-    id: user.id,
-    email: user.email,
-    password: user.password,
-    salt: user.salt,
-    accessToken: user.accessToken,
-  };
+}
 
-  return userDTO;
-};
 
-export const UserModel = mongoose.model<User>("Users", UserSchema);
+// export const getUserByEmail = async (email: string) => {
+//   const user = await UserModel.findOne({ email: email });
+//   if (!user) {
+//     infoLogger({ status: "failed", message: "Error finding user" })
+//     throw new Error("Error finding user");
+//   }
+//   // FIX: replace with UserDTO
+//   const userDTO: User = {
+//     id: user.id,
+//     email: user.email,
+//     password: user.password,
+//     salt: user.salt,
+//     accessToken: user.accessToken,
+//   };
+//
+//   return userDTO;
+// };
+
+export const UserModel = mongoose.model<UserModelSchema>("Users", UserSchema);

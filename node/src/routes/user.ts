0c9106f6a4a03f1fe084d9ddbacd61897184a1ed
Prateek Singh <prateeksingh9741@gmail.com>
2024-12-30 09:59:45 +0530

    cleanup user route to separate auth and user handling logic

--- node/src/routes/user.ts
+++ node/src/routes/user.ts
@@ -1,10 +1,8 @@
 import express, { Router } from "express";
-import { createNewUser, getUser, loginUser } from "@controller/user";
+import { getUser } from "@controller/user";
 
 const router: Router = express.Router();
 
-router.route("/signup").post(createNewUser);
-router.route("/login").post(loginUser);
 router.route("/").post(getUser);
 
 export default router;

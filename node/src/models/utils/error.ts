677c02cc043fe67ee9efc33d1dcc1417dc61e293
Prateek Singh <prateeksingh9741@gmail.com>
2024-12-30 09:58:02 +0530

    minor modifications in error handling pattern

--- node/src/models/utils/error.ts
+++ node/src/models/utils/error.ts
@@ -6,6 +6,7 @@ import { HTTP } from "@senseii/types";
 export const handleDBError = (error: unknown): AppError => {
   // Mongoose-specific error handling.
   if (error instanceof mongoose.Error.ValidationError) {
+    infoLogger({ layer: "DB", status: "failed", message: "Invalid data" })
     return createError(HTTP.STATUS.BAD_REQUEST, 'Invalid data', {
       details: Object.values(error.errors).map(err => err.message)
     });
@@ -13,13 +14,14 @@ export const handleDBError = (error: unknown): AppError => {
 
   // Duplicate key error.
   if (error instanceof mongoose.Error && (error as any).code === 11000) {
+    infoLogger({ layer: "DB", status: "failed", message: "Resource already exists" })
     return createError(HTTP.STATUS.CONFLICT, 'Resource already exists')
   }
 
   // Network Error.
   // Unknown error occured.
   const err = error instanceof Error ? error.message : String(error)
-  infoLogger({ status: "failed", message: err })
+  infoLogger({ layer: "DB", status: "failed", message: err })
   return createError(HTTP.STATUS.NOT_FOUND, 'Unexpected Database Error occured', {
     originalError: err
   })

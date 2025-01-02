49858166148e761c14cebf5329f64ac066c50762
Prateek Singh <prateeksingh9741@gmail.com>
2025-01-02 11:57:16 +0530

    modify types of export required validation object

--- node/src/types.ts
+++ node/src/types.ts
@@ -1,27 +1,20 @@
 import { HTTP, HTTPStatus } from "@senseii/types"
 import z from "zod"
 
-const ZErrorCode = z.enum([
-  'VALIDATION_ERROR',
-  'CONFLICT_ERROR',
-  'NOT_FOUND_ERROR',
-  'INTERNAL_SERVER_ERROR'
-])
-const ZAppError = z.object({
+export const ZAppError = z.object({
   code: z.number(),
   message: z.string(),
   details: z.record(z.unknown()).optional(),
-  timestamp: z.date()
+  timestamp: z.string().datetime()
 })
 
 export type AppError = z.infer<typeof ZAppError>
-type ErrorCode = z.infer<typeof ZErrorCode>
 
 export type Result<T> = { success: true, data: T } | { success: false, error: AppError }
 
 // details field is populated only when I throw error, otherwise message will only contain database error.
 export const createError = (code: HTTPStatus, message: string, details?: Record<string, unknown>): AppError => (
   {
-    code, message, details, timestamp: new Date()
+    code, message, details, timestamp: new Date().toISOString()
   }
 )

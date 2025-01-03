3e2b97bfbb2e6e857fb2c663a6fa3f2244848284
Prateek Singh <prateeksingh9741@gmail.com>
2025-01-03 18:16:07 +0530

    completing user profile schema

--- node/src/models/userProfile.ts
+++ node/src/models/userProfile.ts
@@ -1,7 +1,7 @@
 import chalk from "chalk";
 import { Schema, model, Types } from "mongoose";
 import { infoLogger } from "../utils/logger/logger";
-import { Result, RunRequestDTO, UserProfileModel, userChatsSchema, userProfileModelSchema } from "@senseii/types";
+import { Result, RunRequestDTO, User, UserProfile, UserProfileModel, userChatsSchema, userProfileModelSchema } from "@senseii/types";
 import { z } from "zod";
 import { handleDBError } from "./utils/error";
 
@@ -25,11 +25,35 @@ const IChatSchema: Schema<IChatsDocument> = new Schema({
   },
 });
 
+
+// FIX: This is not the complete user information.
 const UserProfileSchema: Schema<IUserProfileDocument> = new Schema({
   id: {
     type: String,
     ref: "Users",
     required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Email must be provided"],
+  },
+  name: {
+    type: String,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+  verified: {
+    type: Boolean,
+    required: true
   },
   chats: {
     type: [IChatSchema],
@@ -42,8 +66,77 @@ const UserProfileModel = model<IUserProfileDocument>(
 );
 
 export const userProfileStore = {
-  GetThreadByChatId: (data: RunRequestDTO): Promise<Result<Chat>> => getUserThreadId(data)
+  GetThreadByChatId: (data: RunRequestDTO): Promise<Result<Chat>> => getUserThreadId(data),
+  AddChatToUser: (chatId: string, userId: string, threadId: string, summary: string): Promise<Result<null>> => addChatToUser(chatId, userId, threadId, summary),
+  CreateProfile: (user: User) => createUserProfile(user)
 }
+
+const createUserProfile = async (user: User): Promise<Result<null>> => {
+  try {
+    const newProfile = await new UserProfileModel({
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      createdAt: user.createdAt,
+      verified: user.verified,
+      chats: []
+    }).save()
+    return {
+      success: true,
+      data: null
+    }
+  } catch (error) {
+    return {
+      success: false,
+      error: handleDBError(error, "USER PROFILE DATASTORE")
+    }
+  }
+
+}
+
+export const addChatToUser = async (
+  chatId: string,
+  userId: string,
+  threadId: string,
+  summary: string,
+): Promise<Result<null>> => {
+  try {
+    infoLogger({ message: "saving new chat in user profile", status: "INFO", layer: 'DB', name: "USER PROFILE STORE" })
+    const newChat: Chat = {
+      id: chatId,
+      threadId: threadId,
+      summary: summary,
+    };
+
+    if (!Types.ObjectId.isValid(userId)) {
+      throw new Error("Invalid user ID");
+    }
+
+
+    const updatedUserProfile = await UserProfileModel.findOneAndUpdate(
+      { id: userId },
+      { $push: { chats: newChat } },
+      { new: true, useFindAndModify: false },
+    )
+
+    if (!updatedUserProfile) {
+      throw new Error("error updating profile")
+    }
+
+    infoLogger({ message: "Profile was updated", status: "success", layer: "DB", name: "USER PROFILE STORE" });
+    return {
+      success: true,
+      data: null
+    }
+  } catch (error) {
+    return {
+      success: false,
+      error: handleDBError(error, "USER PROFILE STORE")
+    }
+  }
+};
 
 export const saveNewUserProfile = async (user: UserProfileModelSchema) => {
   try {
@@ -120,40 +213,6 @@ export const getUserThreads = async (userId: string) => {
   }
 
   return response?.chats;
-};
-
-export const addChatToUser = async (
-  chatId: string,
-  userId: string,
-  threadId: string,
-  summary: string,
-) => {
-  try {
-    const newChat: Chat = {
-      id: chatId,
-      threadId: threadId,
-      summary: summary,
-    };
-
-    if (!Types.ObjectId.isValid(userId)) {
-      throw new Error("Invalid user ID");
-    }
-
-    const updatedUserProfile = await UserProfileModel.findOneAndUpdate(
-      { id: new Types.ObjectId(userId) },
-      { $push: { chats: newChat } },
-      { new: true, useFindAndModify: false },
-    )
-
-    infoLogger({ message: "Profile was updated" });
-
-    return updatedUserProfile;
-  } catch (error) {
-    console.error(
-      chalk.white(chalk.bgRed("[ERROR]"), "Error updating user profile", error),
-    );
-    throw error;
-  }
 };
 
 export default UserProfileModel;

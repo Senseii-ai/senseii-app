d60d039c262c0d890354d169fc5fe7ea2e6b5c73
Prateek Singh <prateeksingh9741@gmail.com>
2025-01-03 18:15:45 +0530

    level summary: Assistant

--- node/src/services/openai/assistants/summary/utils.ts
+++ node/src/services/openai/assistants/summary/utils.ts
@@ -1,21 +1,56 @@
 import { createRun } from "@services/openai/assistants"
-import getOpenAIClient from "@services/openai/client";
-import { latestMessage } from "@services/openai/utils";
+import { latestMessage, openAIUtils } from "@services/openai/utils";
 import { getSummaryAssistantId } from "@services/openai/assistants/summary"
 import { infoLogger } from "@utils/logger";
+import { AzureOpenAI, OpenAIError } from "openai";
+import { HTTP, Result } from "@senseii/types";
 
-export const summariseChat = async (threadId: string) => {
-  const client = getOpenAIClient();
-  const response = await createRun(threadId, client, getSummaryAssistantId());
-  if (!response) {
-    infoLogger({ message: "Error creating thread summarising run", status: "failed" })
-    return null;
+export const summaryAssistant = {
+  summariseChat: (client: AzureOpenAI, threadId: string, wordLimit: number): Promise<Result<string>> => summariseChat(client, threadId, wordLimit)
+}
+
+export const summariseChat = async (client: AzureOpenAI, threadId: string, wordLimit: number): Promise<Result<string>> => {
+  try {
+    infoLogger({ message: "summarising chat", status: "INFO", layer: "SERVICE", name: "OAI:SUMMARISE CHAT" })
+    const newMessage = openAIUtils.CreateMessage(`<WORD LIMIT>: ${wordLimit}`, "user")
+    await openAIUtils.AddMessageToThread(client, threadId, newMessage)
+    const response = await createRun(threadId, client, getSummaryAssistantId());
+    if (!response) {
+      infoLogger({ message: "error creating thread summarising run", status: "failed", layer: "SERVICE", name: "OAI:SUMMARIZE CHAT" })
+      throw new OpenAIError("AI Provider error")
+    }
+    const summary = latestMessage(response[0]);
+    if (!summary) {
+      infoLogger({ message: "Error generating chat summary", status: "failed", layer: "SERVICE", name: "OAI:SUMMARIZE CHAT" })
+      throw new OpenAIError("error generating summary")
+    }
+    const instructionMessage = latestMessage(response[1])
+    // delete the summary message from thread
+    await client.beta.threads.messages.del(threadId, summary?.messageId);
+    await client.beta.threads.messages.del(threadId, instructionMessage?.messageId as string);
+    return {
+      success: true,
+      data: summary.content
+    }
+  } catch (error) {
+    if (error instanceof OpenAIError) {
+      return {
+        success: false,
+        error: {
+          code: HTTP.STATUS.INTERNAL_SERVER_ERROR,
+          message: error.message,
+          timestamp: new Date().toISOString(),
+          details: { stack: error.stack }
+        }
+      }
+    }
+    return {
+      success: false,
+      error: {
+        code: HTTP.STATUS.INTERNAL_SERVER_ERROR,
+        message: "unknown error occured",
+        timestamp: new Date().toISOString()
+      }
+    }
   }
-  const summary = latestMessage(response[0]);
-  if (!summary) {
-    throw new Error("Error summarising thread");
-  }
-  // delete the summary message from thread
-  await client.beta.threads.messages.del(threadId, summary?.messageId);
-  return summary.content;
 };

5e0962a4a4315073cbe25102c88b0ed1859e3b39
Prateek Singh <prateeksingh9741@gmail.com>
2025-01-04 09:48:23 +0530

    add getAllUserChats route

--- node/src/routes/chat.ts
+++ node/src/routes/chat.ts
@@ -1,15 +1,12 @@
 import express, { Router } from "express";
 import {
-  // getChats,
-  // getChatMessages,
   openAIController,
 } from "../controller/chat";
-import { openAIService } from "@services/openai/service";
 
 const router: Router = express.Router();
 
 router.route("/").post(openAIController.Chat);
 // router.route("/user/:userId/chat/:chatId").get(getChatMessages);
-// router.route("/user/:userId/chats").get(getChats);
+router.route("/user/:email/chats").get(openAIController.GetChats);
 
 export default router;

2ebbe9a5b3d51c0f395c59d983ca32924e000455
Prateek Singh <prateeksingh9741@gmail.com>
2025-01-12 15:52:05 +0530

    add webhook handler in the backend

--- node/src/server.ts
+++ node/src/server.ts
@@ -1,15 +1,15 @@
 import "tsconfig-paths"
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 require("dotenv").config();
 import connectDB from "@db/connect";
-import { userRouter, vitalsRouter, chatRouter, healthRouter, threadsRouter, authRouter } from "@routes";
+import { userRouter, vitalsRouter, chatRouter, authRouter } from "@routes";
 import cors from "cors";
-import { authenticateUser } from "middlewares/auth";
 import bodyParser from "body-parser";
-// import { createAllAssistants } from "@services/openai/utils";
 import { infoLogger } from "@utils/logger";
 import swaggerUi from "swagger-ui-express"
 import { swaggerDocs } from "@utils/swagger";
+import { clerkMiddleware, requireAuth } from "@clerk/express";
+import { Webhook } from "svix";
 
 const app: Express = express();
 app.use(cors());
@@ -20,23 +20,79 @@ app.use(
 );
 const port = 9090;
 
-app.use(express.json());
-app.use(authRouter);
-app.use("/user", userRouter)
+// app.use(express.json());
+app.use(clerkMiddleware())
+app.post(
+  '/api/webhooks',
+  bodyParser.raw({ type: 'application/json' }),
+  async (req, res) => {
+    infoLogger({ message: "New user created", status: "alert" })
+    const SIGNING_SECRET = process.env.SIGNING_SECRET
+
+    if (!SIGNING_SECRET) {
+      throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env')
+    }
+
+    // Create new Svix instance with secret
+    const wh = new Webhook(SIGNING_SECRET)
+
+
+    // Get headers and body
+    const headers = req.headers
+    const payload = req.body
+
+    // Get Svix headers for verification
+    const svix_id = headers['svix-id']
+    const svix_timestamp = headers['svix-timestamp']
+    const svix_signature = headers['svix-signature']
+
+    // If there are no headers, error out
+    if (!svix_id || !svix_timestamp || !svix_signature) {
+      return void res.status(400).json({
+        success: false,
+        message: 'Error: Missing svix headers',
+      })
+    }
+
+    let evt: any
+
+    // Attempt to verify the incoming webhook
+    // If successful, the payload will be available from 'evt'
+    // If verification fails, error out and return error code
+    try {
+      evt = wh.verify(payload, {
+        'svix-id': svix_id as string,
+        'svix-timestamp': svix_timestamp as string,
+        'svix-signature': svix_signature as string,
+      })
+    } catch (err) {
+      console.log('Error: Could not verify webhook:', err)
+      return void res.status(400).json({
+        success: false,
+        message: err,
+      })
+    }
+
+    // Do something with payload
+    // For this guide, log payload to console
+    const { id } = evt.data
+    const eventType = evt.type
+    if (evt.type === 'user.created') {
+      console.log('userId:', evt.data.id)
+    }
+
+    return void res.status(200).json({
+      success: true,
+      message: 'Webhook received',
+    })
+  },
+)
+
 // FIX: for production drop this route below authenticator.
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
-app.use(authenticateUser);
-app.use("/api/vitals", vitalsRouter);
-app.use("/chat", chatRouter);
-app.use("/api/health", healthRouter);
-// createAllAssistants()
-
-// list of test routers
-app.use("/api/threads", threadsRouter);
-
-app.get("/ping", (req: Request, res: Response) => {
-  res.send("pong");
-});
+app.use("/user", requireAuth(), userRouter)
+app.use("/api/vitals", requireAuth(), vitalsRouter);
+app.use("/chat", requireAuth(), chatRouter);
 
 const start = async () => {
   await connectDB();

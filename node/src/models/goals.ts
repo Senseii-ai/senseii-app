b76408ec1ddf5a8f3c9762bc1c9430da92957569
Prateek Singh <prateeksingh9741@gmail.com>
2025-01-16 21:14:23 +0530

    modify goals model and user model

--- node/src/models/goals.ts
+++ node/src/models/goals.ts
@@ -1,28 +1,17 @@
-import { z } from "zod"
 import { Schema, model, Document } from "mongoose"
-import { INutritionPlan, IBasicInformation, IConstraints, IDietPreferences, IEatingHabits, IHealthGoals, ILifeStyle, IInitialGoal, IThreads, IUserGoal, UserGoalDTO } from "@senseii/types";
-import { UserPreferencesSchema } from "@models/userPreference";
-import { NutritionPlanSchema } from "@models/nutritionPlan";
-import { validateResponse } from "@services/openai/utils";
-import { getUserId } from "middlewares/auth";
-import { infoLogger } from "@utils/logger";
+import { CreateUserGoalDTO, UserGoal } from "./temp.types";
+import { IBasicInformation, IConstraints, IDietPreferences, IEatingHabits, IHealthGoals, ILifeStyle, NutritionPlan } from "@senseii/types";
+import { getUserId } from "@middlewares/auth";
 
-interface IUserGoalDocument extends IUserGoal, Document { }
+interface UserGoalDocument extends UserGoal, Document { }
 
-const ThreadsSchema = new Schema<IThreads>({
-  coreThreadId: { type: String, required: true },
-  nutritionThreadId: { type: String, required: true },
-  workoutThreadId: { type: String, required: true }
-})
-
-// include the field for isActive.
-const UserGoalSchema = new Schema<IUserGoalDocument>({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "Users",
+const UserGoalSchema: Schema<UserGoalDocument> = new Schema({
+  userId: {
+    type: String,
     required: true,
+    ref: "UserProfile"
   },
-  goalName: {
+  title: {
     type: String,
     required: true,
   },
@@ -30,93 +19,97 @@ const UserGoalSchema = new Schema<IUserGoalDocument>({
     type: String,
     required: true,
   },
-  preferences: {
-    type: UserPreferencesSchema,
-  },
-  nutritionPlan: {
-    type: NutritionPlanSchema,
+  chatId: {
+    type: String,
+    required: true,
+    ref: "Chats"
   },
   workoutPlan: {
     type: String,
+    // FIX: Connect when workout plans model is done.
   },
   startDate: {
     type: String,
   },
   endDate: {
-    type: Date,
+    type: String,
   },
-  threads: {
-    type: ThreadsSchema,
+  nutritionPlan: {
+    type: String,
+    ref: "NutritionPlans"
   },
 })
 
-export const UserGoal = model<IUserGoalDocument>('UserGoal', UserGoalSchema)
+export const UserGoalModel = model<UserGoalDocument>('Goals', UserGoalSchema)
 
 // NOTE: mayne this can be improved in the future.
 // updateBasicInformation updates the basic information field of a user document
 export const saveUpdatedBasicInformaion = async (data: IBasicInformation) => {
-  return await UserGoal.updateOne({ user: getUserId() }, { $set: { ["preferences.basicInformation"]: data } })
+  return await UserGoalModel.updateOne({ user: getUserId() }, { $set: { ["preferences.basicInformation"]: data } })
 }
 
 export const saveUpdatedDietPreferences = async (data: IDietPreferences) => {
-  return await UserGoal.updateOne({ user: getUserId() }, { $set: { ["preferences.dietPreferences"]: data } })
+  return await UserGoalModel.updateOne({ user: getUserId() }, { $set: { ["preferences.dietPreferences"]: data } })
 }
 
 export const saveUpdatedEatingHabits = async (data: IEatingHabits) => {
-  return await UserGoal.updateOne({ user: getUserId() }, { $set: { ["preferences.eatingHabits"]: data } })
+  return await UserGoalModel.updateOne({ user: getUserId() }, { $set: { ["preferences.eatingHabits"]: data } })
 }
 
-export const saveNutritionPlan = async (data: INutritionPlan) => {
-  return await UserGoal.updateOne({ user: getUserId() }, { $set: { ["nutritionPlan"]: data } })
+export const saveNutritionPlan = async (data: NutritionPlan) => {
+  return await UserGoalModel.updateOne({ user: getUserId() }, { $set: { ["nutritionPlan"]: data } })
 }
 
 export const saveUpdatedUserConstraints = async (data: IConstraints) => {
-  return await UserGoal.updateOne({ user: getUserId() }, { $set: { ["preferences.constraints"]: data } })
+  return await UserGoalModel.updateOne({ user: getUserId() }, { $set: { ["preferences.constraints"]: data } })
 }
 
-export const saveInitialGoal = async (data: IInitialGoal) => {
-  const userGoalData: IUserGoal = {
-    user: getUserId(),
-    goalName: data.goalName,
+export const saveInitialGoal = async (data: CreateUserGoalDTO) => {
+  const userGoalData: CreateUserGoalDTO = {
+    userId: getUserId(),
+    title: data.title,
     description: data.description,
-    endDate: data.endDate
+    startDate: data.startDate,
+    endDate: data.endDate,
+    chatId: data.chatId
   }
-  const newGoal = new UserGoal(userGoalData)
+  const newGoal = new UserGoalModel(userGoalData)
   return await newGoal.save()
 }
 
 export const saveUpdateUserHealthGoals = async (data: IHealthGoals) => {
-  return await UserGoal.updateOne({ user: getUserId() }, { ["preferences.healthGoals"]: data })
+  return await UserGoalModel.updateOne({ user: getUserId() }, { ["preferences.healthGoals"]: data })
 }
 
 export const saveUpdatedLifeStyle = async (data: ILifeStyle) => {
-  return await UserGoal.updateOne({ user: getUserId() }, { ["preferences.lifeStyle"]: data })
+  return await UserGoalModel.updateOne({ user: getUserId() }, { ["preferences.lifeStyle"]: data })
 }
 
 // createUserGoal
-export const saveUserGoal = async (args: string): Promise<IUserGoalDocument> => {
-  try {
-    // NOTE: currently gpt-4o doesn't support structured function calling, so this implementation 
-    // is a workaround using gpt-4o-mini chat completions.
-    infoLogger({ status: "alert", message: "calling create-goal function" })
-    const validatedResponse = await validateResponse({ prompt: args, validatorSchema: UserGoalDTO, validatorSchemaName: "create-user-goal" })
-    const userId = getUserId()
-    const data: IUserGoal = {
-      user: userId,
-      preferences: validatedResponse.preferences,
-      goalName: validatedResponse.goalName,
-      description: validatedResponse.description,
-      startDate: Date.now().toLocaleString()
-    }
-    // NOTE: maybe this is useless check
-    const validatedData = UserGoalDTO.parse(data)
-    infoLogger({ message: "user goal valid", status: "INFO" })
-    const userGoal = new UserGoal(validatedData)
-    return await userGoal.save()
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      throw new Error(`Validation Error, ${error.message}`)
-    }
-    throw error
-  }
-}
+// FIX: This needs to be fixed.
+// export const saveUserGoal = async (args: string): Promise<UserGoalDocument> => {
+//   try {
+//     // NOTE: currently gpt-4o doesn't support structured function calling, so this implementation 
+//     // is a workaround using gpt-4o-mini chat completions.
+//     infoLogger({ status: "alert", message: "calling create-goal function" })
+//     const validatedResponse = await validateResponse({ prompt: args, validatorSchema: UserGoalDTO, validatorSchemaName: "create-user-goal" })
+//     const userId = getUserId()
+//     const data: CreateUserGoalDTO= {
+//       userId: userId,
+//       preferences: validatedResponse.preferences,
+//       title: validatedResponse.goalName,
+//       description: validatedResponse.description,
+//       startDate: Date.now().toLocaleString()
+//     }
+//     // NOTE: maybe this is useless check
+//     const validatedData = UserGoalDTO.parse(data)
+//     infoLogger({ message: "user goal valid", status: "INFO" })
+//     const userGoal = new UserGoalModel(validatedData)
+//     return await userGoal.save()
+//   } catch (error) {
+//     if (error instanceof z.ZodError) {
+//       throw new Error(`Validation Error, ${error.message}`)
+//     }
+//     throw error
+//   }
+// }
